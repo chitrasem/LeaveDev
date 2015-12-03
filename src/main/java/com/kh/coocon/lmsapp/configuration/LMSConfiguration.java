badd430c67e2257a45f343016d64870bdf78437b
Ryna123 <yonryna456@gmail.com>
2015-12-03 13:49:20 +0700

    lmsConfiguration

--- src/main/java/com/kh/coocon/lmsapp/configuration/LMSConfiguration.java
+++ src/main/java/com/kh/coocon/lmsapp/configuration/LMSConfiguration.java
@@ -1,8 +1,11 @@
 package com.kh.coocon.lmsapp.configuration;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -33,6 +36,19 @@ public class LMSConfiguration extends WebMvcConfigurerAdapter{
 	       org.springframework.web.multipart.commons.CommonsMultipartResolver multipartResolver = new org.springframework.web.multipart.commons.CommonsMultipartResolver();
 	       multipartResolver.setMaxUploadSize(2097152);
 	       return multipartResolver;
+	}
+	
+	@Bean
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+//		dataSource.setUrl("jdbc:postgresql://ec2-54-83-202-64.compute-1.amazonaws.com:5432/d52hue0higch0t?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory");
+//		dataSource.setUsername("jvqiqzwlgblztv");
+//		dataSource.setPassword("f_j9jAsT9C_WzHwVDrGYj1_3op");
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/WeCafe");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("admin");
+		return dataSource;
 	}
 	
 }
