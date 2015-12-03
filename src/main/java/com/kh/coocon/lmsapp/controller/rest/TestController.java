a14f344df9eea3a47b4338aeec8d747463eb0c1d
Ryna123 <yonryna456@gmail.com>
2015-12-03 14:50:39 +0700

    aa

--- src/main/java/com/kh/coocon/lmsapp/controller/rest/TestController.java
+++ src/main/java/com/kh/coocon/lmsapp/controller/rest/TestController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class TestController {
 	@Autowired
 	UserTestService userService;
 	
-	@RequestMapping(value = { "/"}, method = RequestMethod.GET ,  produces=MediaType.APPLICATION_JSON_VALUE)
+	/*@RequestMapping(value = { "/"}, method = RequestMethod.GET ,  produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Map<String, Object>> list() {
 		List<UserTest> list = userService.list();
 		Map<String, Object> map = new HashMap<String, Object>();
@@ -32,5 +33,11 @@ public class TestController {
 		}
 		map.put("RESP_DATA", list);
 		return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
+	}*/
+	
+	@RequestMapping(value={"/","/dashboard","/home","/index"} , method = RequestMethod.GET)
+	public String dasboardPage(ModelMap m){
+		m.addAttribute("message","Dashboard");
+		return "admin";
 	}
 }
