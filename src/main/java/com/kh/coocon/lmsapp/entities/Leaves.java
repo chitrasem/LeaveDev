ed8065c441b8e8cb69cc9f2087eb3e304fdd4dec
Ryna123 <yonryna456@gmail.com>
2015-12-09 16:51:02 +0700

    aa

--- src/main/java/com/kh/coocon/lmsapp/entities/Leaves.java
+++ src/main/java/com/kh/coocon/lmsapp/entities/Leaves.java
@@ -1,5 +1,46 @@
 package com.kh.coocon.lmsapp.entities;
 
 public class Leaves {
-
+	private String leavesStartdate;
+	private String leavesEnddate;
+	private String leavesDuration;
+	private String leavesReason;
+	private String leavsTypes;
+	private String leavesStatus;
+	public String getLeavesStartdate() {
+		return leavesStartdate;
+	}
+	public void setLeavesStartdate(String leavesStartdate) {
+		this.leavesStartdate = leavesStartdate;
+	}
+	public String getLeavesEnddate() {
+		return leavesEnddate;
+	}
+	public void setLeavesEnddate(String leavesEnddate) {
+		this.leavesEnddate = leavesEnddate;
+	}
+	public String getLeavesDuration() {
+		return leavesDuration;
+	}
+	public void setLeavesDuration(String leavesDuration) {
+		this.leavesDuration = leavesDuration;
+	}
+	public String getLeavesReason() {
+		return leavesReason;
+	}
+	public void setLeavesReason(String leavesReason) {
+		this.leavesReason = leavesReason;
+	}
+	public String getLeavsTypes() {
+		return leavsTypes;
+	}
+	public void setLeavsTypes(String leavsTypes) {
+		this.leavsTypes = leavsTypes;
+	}
+	public String getLeavesStatus() {
+		return leavesStatus;
+	}
+	public void setLeavesStatus(String leavesStatus) {
+		this.leavesStatus = leavesStatus;
+	}
 }
