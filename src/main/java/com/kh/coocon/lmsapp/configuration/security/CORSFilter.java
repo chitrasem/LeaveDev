d7f41f0916a0262b369580dbfb5a09714cee5433
Ryna123 <yonryna456@gmail.com>
2015-12-14 13:30:16 +0700

    aa

--- src/main/java/com/kh/coocon/lmsapp/configuration/security/CORSFilter.java
+++ src/main/java/com/kh/coocon/lmsapp/configuration/security/CORSFilter.java
@@ -8,13 +8,16 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
 
+@WebFilter(urlPatterns="/admin/*")
 public class CORSFilter implements Filter {
 	 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         System.out.println("Filtering on...........................................................");
         HttpServletResponse response = (HttpServletResponse) res;
+       // HttpServletResponse response = (HttpServletResponse) Response;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
