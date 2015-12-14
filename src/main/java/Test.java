c2fe336f823b7cdafa1873b74c8290b6e8f2e71a
Ryna123 <yonryna456@gmail.com>
2015-12-14 14:55:04 +0700

    commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,15 +3,5 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 
 public class Test {
-	 private String getPrincipal(){
-    	 String userName = null;
-         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-  
-         if (principal instanceof UserDetails) {
-             userName = ((UserDetails)principal).getUsername();
-         } else {
-             userName = principal.toString();
-         }
-         return userName;
-     }
+	
 }
